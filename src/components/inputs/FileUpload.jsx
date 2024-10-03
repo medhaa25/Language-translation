c4f616c9c16ca3684331803a89cdc27b1feb029d
medhaa25 <medharwt25.2000@gmail.com>
2024-10-03 10:47:32 +0530

    first commit

---
+++ src/components/inputs/FileUpload.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { IconPaperclip } from "@tabler/icons-react";
+
+const FileUpload = ({ handleFileUpload }) => {
+  return (
+    <label htmlFor="file-upload" className="cursor-pointer">
+      <IconPaperclip size={21} />
+      <input
+        id="file-upload"
+        type="file"
+        onChange={handleFileUpload}
+        className="hidden"
+      />
+    </label>
+  );
+};
+
+export default FileUpload;
---
+++ src/components/inputs/LinkPaste.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { IconLink } from "@tabler/icons-react";
+
+const LinkPaste = ({ handleLinkPaste }) => {
+  return (
+    <label htmlFor="link-input" className="cursor-pointer">
+      <IconLink size={21} />
+      <input
+        type="text"
+        id="link-input"
+        className="hidden"
+        onChange={handleLinkPaste}
+      />
+    </label>
+  );
+};
+
+export default LinkPaste;
